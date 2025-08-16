5adef72fdd068077fbe3898456c1d8c6ea53c69a
Cem Bıçakcı <cmbicakci@gmail.com>
2025-08-16 16:23:21 +0300

    initial commit

---
+++ TravelAppUI/Extensions/Image.swift
@@ -0,0 +1,14 @@
+//
+//  Image.swift
+//  TravelAppUI
+//
+//  Created by Cem Bıçakcı on 16.08.2025.
+//
+
+import SwiftUI
+
+extension Image {
+    static var beach_image_1: Image {
+        Image("beach_image_1")
+    }
+}
---
+++ TravelAppUI/TravelAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppUIApp.swift
+//  TravelAppUI
+//
+//  Created by Cem Bıçakcı on 16.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
