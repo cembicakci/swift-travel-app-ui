9695ec34005684a50243404642f2c6cb5cbeba09
Cem Bıçakcı <cmbicakci@gmail.com>
2025-08-16 16:25:01 +0300

    create color extension

---
+++ TravelAppUI/Extensions/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  TravelAppUI
+//
+//  Created by Cem Bıçakcı on 16.08.2025.
+//
+
+
+import SwiftUI
+
+extension Color {
+    static var appBlueColor: Color {
+        Color("AppBlueColor")
+    }
+}
--- TravelAppUI/Extensions/Image.swift
+++ TravelAppUI/Extensions/Image.swift
@@ -7,8 +7,77 @@
 
 import SwiftUI
 
+
 extension Image {
     static var beach_image_1: Image {
         Image("beach_image_1")
+    }
+    
+    static var land_image_1: Image {
+        Image("land_image_1")
+    }
+    
+    static var location: Image {
+        Image("location")
+    }
+    
+    static var menu: Image {
+        Image("menu")
+    }
+    
+    static var mountain_icon: Image {
+        Image("mountain_icon")
+    }
+    
+    static var mountain_image_1: Image {
+        Image("mountain_image_1")
+    }
+    
+    static var mountain_image_2: Image {
+        Image("mountain_image_2")
+    }
+    
+    static var river_icon: Image {
+        Image("river_icon")
+    }
+    
+    static var tower_image_1: Image {
+        Image("tower_image_1")
+    }
+    
+    static var user: Image {
+        Image("user")
+    }
+    
+    static var waterfall_icon: Image {
+        Image("waterfall_icon")
+    }
+    
+    static var right_arrow: Image {
+        Image("right_arrow")
+    }
+    
+    static var heart: Image {
+        Image("heart")
+    }
+    
+    static var left_arrow: Image {
+        Image("left_arrow")
+    }
+    
+    static var preview_image_1: Image {
+        Image("preview_image_1")
+    }
+    
+    static var preview_image_2: Image {
+        Image("preview_image_2")
+    }
+    
+    static var preview_image_3: Image {
+        Image("preview_image_3")
+    }
+    
+    static var preview_image_4: Image {
+        Image("preview_image_4")
     }
 }
